9046afe46e2a540bf1d9ad0b370a049739258f62
Peter Jaros <peter.a.jaros@gmail.com>
2018-01-28 13:28:44 -0500

    Store results in a database

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
+var level = require("level");
+var db = level("./db");
+
 const fetchOvercastHTML = require("./fetchOvercastHTML");
 const timeInMinutes = require("./timeInMinutes");
 
 module.exports = () =>
-  fetchOvercastHTML(process.env.COOKIE).then(timeInMinutes);
+  fetchOvercastHTML(process.env.COOKIE)
+    .then(timeInMinutes)
+    .then(mins => db.put(new Date(), mins));
